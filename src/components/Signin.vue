c7f41457bc72a4c9fbda5103a91ed56ce58ad357
darinmarinov <darinmariqnovmarinov@windowslive.com>
2018-07-22 23:07:45 +0300

    Made Sign In Form

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,5 +1,42 @@
 <template>
-<div class="">
-    Sign in
+<div class="col-sm-6 col-sm-auto justify-content-center">
+ <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+  </div>
+  <button type="submit" class="btn btn-primary" @click.prevent='signIn'>Sign In</button>
+ </form>
+ <br>
+ <br>
+ <p>{{error.message}}</p>
 </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp'
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error:{
+          message:''
+      }
+    };
+  },
+  methods:{
+      signIn(){
+          firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+          .catch(error=>{
+              this.error = error
+          })
+      }
+  }
+};
+</script>
